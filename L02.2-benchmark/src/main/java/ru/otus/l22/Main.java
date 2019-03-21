a66f13d723b8c64196708b52f1eca98f4e77f9f7
y0k <y0kzelan@gmail.com>
2019-03-21 15:35:45 +0300

    HW02.2-benchmark added Main & Benchmark classes

--- L02.2-benchmark/src/main/java/ru/otus/l22/Main.java
+++ L02.2-benchmark/src/main/java/ru/otus/l22/Main.java
@@ -1,4 +1,24 @@
 package ru.otus.l22;
 
+import java.lang.management.ManagementFactory;
+        //vm options -Xmx1g -Xms1g
 public class Main {
+
+    public static void main(String... args) {
+        System.out.println("Start pid: " + ManagementFactory.getRuntimeMXBean().getName());
+        Benchmark benchmark = new  Benchmark();
+        benchmark.prepare();
+
+        benchmark.measure(Object::new, "Object" );
+        benchmark.clean();
+
+        benchmark.measure(String::, "String" );
+        benchmark.clean();
+
+        benchmark.measure(() -> new String(new byte[0])), "String of list bytes");
+        benchmark.clean();
+
+        benchmark.measure(() -> new Benchmark(10), "Benchmark(10)");
+        benchmark.clean();
+    }
 }
