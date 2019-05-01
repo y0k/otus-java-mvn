9e537aab8ee07ab87f372f030b2c815d1c0f7109
Kir <y0kzelan@gmail.com>
2019-05-01 14:48:44 +0300

    hw01-maven final

--- One/src/main/java/ru/otus/l011/HelloOtus.java
+++ One/src/main/java/ru/otus/l011/HelloOtus.java
@@ -8,9 +8,9 @@ import com.google.common.base.Splitter;
  *
  * To start the application:
  * mvn package
- * java -cp ./target/L01.1-maven.jar ru.otus.l011.Main
+ * java -cp ./target/L01.1-maven.jar ru.otus.l011.HelloOtus
  * java -jar ./target/L01.1-maven.jar //java.lang.NoClassDefFoundError: com/google/common/collect/Lists
- * java -cp ./target/L01.1-maven.jar;C:\Users\Tully\.m2\repository\com\google\guava\guava\25.0-jre\guava-25.0-jre.jar ru.otus.l011.Main
+ * java -cp ./target/L01.1-maven.jar;C:\Users\User\.m2\repository\com\google\guava\guava\25.0-jre\guava-25.0-jre.jar ru.otus.l011.HelloOtus
  *
  * To unzip the jar:
  * 7z x -oJAR ./target/L01.1-maven.jar
@@ -23,7 +23,7 @@ import com.google.common.base.Splitter;
  * mvn clean compile assembly:single
  */
                                
-public class Main {
+public class HelloOtus {
     public static void main(String[] args) {
         String str = ",,,bam,  ,  bum,, bah   ,nah";
         
