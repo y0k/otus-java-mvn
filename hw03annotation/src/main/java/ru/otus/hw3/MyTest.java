ce8eec2237105d4576d5ead9326683712b1503d6
Cy <y0kzelan@gmail.com>
2019-06-15 13:37:29 +0300

    hw3 start

---
+++ hw03annotation/src/main/java/ru/otus/hw3/MyTest.java
@@ -0,0 +1,15 @@
+package ru.otus.hw3;
+
+public class MyTest {
+
+    public MyTest() {
+        System.out.println("MyTest");
+    }
+    @Before
+    public void set1() {
+        System.out.println("Before 1");
+    }
+    @Before
+
+
+}
