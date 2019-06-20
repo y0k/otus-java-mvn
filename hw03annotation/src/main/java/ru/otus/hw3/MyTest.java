e364c3fc9f6a70477a9dfba997953258be15b5f1
Cy <y0kzelan@gmail.com>
2019-06-20 15:49:18 +0300

    1.1

--- hw03annotation/src/main/java/ru/otus/hw3/MyTest.java
+++ hw03annotation/src/main/java/ru/otus/hw3/MyTest.java
@@ -5,11 +5,44 @@ public class MyTest {
     public MyTest() {
         System.out.println("MyTest");
     }
+
     @Before
-    public void set1() {
+    public void setUp1() {
         System.out.println("Before 1");
     }
+
     @Before
+    public void setUp2() {
+        System.out.println("Before 2");
+    }
 
+    @Test
+    public void test1() {
+        System.out.println("test1");
+    }
 
+    @Test
+    public void test2() {
+        System.out.println("test2");
+    }
+
+    @Test
+    public void test3() {
+        System.out.println("test3"); throw new RuntimeException();
+    }
+
+    @Test
+    private void test4() {
+        System.out.println("test4");
+    }
+
+    @After
+    public void teadDown1() {
+        System.out.println("After 1");
+    }
+
+    @After
+    public void teadDown2() {
+        System.out.println("After 2");
+    }
 }
