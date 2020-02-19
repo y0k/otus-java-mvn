b1c39e55e279cdd4c3d0a6eedc8ebfabd557d385
Kirill <y0kzelan@gmail.com>
2020-02-19 13:23:18 +0300

    create array for each Nominal

---
+++ Hw6Atm/src/main/java/ru/otus/Hw6Atm/Cassete.java
@@ -0,0 +1,11 @@
+package ru.otus.Hw6Atm;
+
+import java.util.ArrayList;
+
+public class Cassete {
+    ArrayList<Nominal> casTen = new ArrayList<>();
+    ArrayList<Nominal> casFift = new ArrayList<>();
+    ArrayList<Nominal> casHundr = new ArrayList<>();
+    ArrayList<Nominal> casFiveHundr = new ArrayList<>();
+    ArrayList<Nominal> casThous = new ArrayList<>();
+}
