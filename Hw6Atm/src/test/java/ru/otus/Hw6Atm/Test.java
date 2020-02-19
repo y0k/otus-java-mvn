29e27555876ba29aba83dd384fbae466747d76a6
Kirill <y0kzelan@gmail.com>
2020-02-19 13:50:42 +0300

    some usage

---
+++ Hw6Atm/src/test/java/ru/otus/Hw6Atm/Test.java
@@ -0,0 +1,26 @@
+package ru.otus.Hw6Atm;
+
+public class Test {
+    public static void main(String[] args) {
+        Nominal[] newsum = {Nominal.TEN, Nominal.TEN, Nominal.HUNDRED, Nominal.HUNDRED, Nominal.FIFTY, Nominal.THOUSAND, Nominal.THOUSAND, Nominal.HUNDRED, Nominal.FIVE_HUNDRED};
+        Atm atm = new Atm();
+        atm.insertMoney(newsum);
+        System.out.println("Всего средств: " + atm.totalMoney());
+        System.out.println("<>Нужно 20<>");
+        atm.printMoney(20);
+        System.out.println("<>Нужно 90<>");
+        atm.printMoney(90);
+        System.out.println("<>Нужно 300<>");
+        atm.printMoney(300);
+        System.out.println("<>Нужно 1560<>");
+        atm.printMoney(1560);
+        System.out.println("<>Нужно 499<>");
+        atm.printMoney(499);
+        System.out.println("<>Текущее состояние счета<>");
+        atm.printAllMoney();
+        System.out.println("<>Нужно 1000<>");
+        atm.printMoney(1000);
+        System.out.println("<>Текущее состояние счета<>");
+        atm.printAllMoney();
+    }
+}
